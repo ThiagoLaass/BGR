e2dca3bdd8ca1547b12cde7036716ef55cf586a7
ThiagoLaass <thiagolaass202@gmail.com>
2024-07-03 18:41:53 -0300

    initiating projcet

---
+++ BeyondGravityRealm/src/main/java/com/BGR/BeyondGravityRealm/BeyondGravityRealmApplication.java
@@ -0,0 +1,13 @@
+package com.BGR.BeyondGravityRealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondGravityRealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondGravityRealmApplication.class, args);
+	}
+
+}
---
+++ BeyondGravityRealm/src/test/java/com/BGR/BeyondGravityRealm/BeyondGravityRealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BGR.BeyondGravityRealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondGravityRealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
